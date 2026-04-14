12ffefa4335f16f6cd0f3fd8886668720ff3729d
phamleanhkhoa101 <phamleanhkhoa2005@gmail.com>
2026-04-14 11:13:20 +0700

    them anh

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,6 @@ import 'package:phamleanhkhoa_6451070135_btlt7/cau1/app/my_app_1.dart';
 import 'package:phamleanhkhoa_6451070135_btlt7/cau2/app/my_app_2.dart';
 
 void main() {
-  runApp(const MyApp1());
-  // runApp(const MyApp2());
+  // runApp(const MyApp1());
+  runApp(const MyApp2());
 }
